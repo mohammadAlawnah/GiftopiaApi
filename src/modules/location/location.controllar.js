2f46f20670e7897ab95d7c3350e2c9e9d8850db1
ReemaKusa <s12112910@stu.najah.edu>
2024-04-30 21:42:49 +0300

    add location, delete location,update location

--- src/modules/location/location.controllar.js
+++ src/modules/location/location.controllar.js
@@ -1,5 +1,24 @@
 import locationModel from "../../../DB/model/Location.model.js";
-export const test = (req,res)=>{
-    console.log('x')
+//import userModel from "../../../DB/model/User.model";
+
+
+export const addlocation=async(req,res)=>{
+
+    const { country, city, street } = req.body;
+    const id = req.user_id;
+
+    const addloc = await locationModel.create({ country, city, street, userId:id})
+    res.json({message: addloc})
 }
 
+export const displayLocation = async(req,res)=>{
+    const location = await locationModel.findone({userId:req.user_id})
+    return res.json({message:location});
+}
+
+export const updateLocation = async(req,res)=>{
+    const { country, city, street } = req.body;
+
+    const location = await locationModel.updateOne({userId:req.user_id},{ country, city, street})
+    return res.json({message:location});
+}
