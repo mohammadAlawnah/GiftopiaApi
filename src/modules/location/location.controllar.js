9a782f699a15809052549580b81c6db3a6cfa659
ReemaKusa <s12112910@stu.najah.edu>
2024-05-01 23:26:19 +0300

    editing

--- src/modules/location/location.controllar.js
+++ src/modules/location/location.controllar.js
@@ -1,24 +1,23 @@
 import locationModel from "../../../DB/model/Location.model.js";
-//import userModel from "../../../DB/model/User.model";
 
 
 export const addlocation=async(req,res)=>{
 
     const { country, city, street } = req.body;
-    const id = req.user_id;
+    const id = req.user._id;
 
     const addloc = await locationModel.create({ country, city, street, userId:id})
     res.json({message: addloc})
 }
 
 export const displayLocation = async(req,res)=>{
-    const location = await locationModel.findone({userId:req.user_id})
+    const location = await locationModel.findone({userId:req.user._id})
     return res.json({message:location});
 }
 
 export const updateLocation = async(req,res)=>{
     const { country, city, street } = req.body;
 
-    const location = await locationModel.updateOne({userId:req.user_id},{ country, city, street})
+    const location = await locationModel.updateOne({userId:req.user._id},{ country, city, street})
     return res.json({message:location});
 }
