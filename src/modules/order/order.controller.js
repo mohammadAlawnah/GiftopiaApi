25cfa3c7c80467d686c60440895e7615a8973276
Rajaa-Ayyash <rajaaayyash43@gmail.com>
2024-05-09 17:46:53 +0300

    Make some modifications to (display, add, and modify tracking)

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -1,41 +1,57 @@
 import orderModel from "../../../DB/model/Order.model.js";
 
-    export const DisplayTraking =async(req,res)=>{
-    try{const orders=await orderModel.findOne();
-    
-    return res.json({message:'succes',orders})
-    
-    }catch(error){
-        return res.json({message:'error',error:error.stack})
-    }
-    }
+export const DisplayTraking = async (req, res) => {
+  try {
+    const orders = await orderModel.findOne({ userId: req.user._id });
+    console.log(orders);
+    return res.json({ message: "succes", orders });
+  } catch (error) {
+    return res.json({ message: "error", error: error.stack });
+  }
+};
 
-    export const addTraking=async(req,res)=>{
-        try{
-            const {product} =req.params;
-            const {status,timeRemaining,totalPrice}=req.body;
-            const order=await orderModel.create({_product:product},{status,timeRemaining,totalPrice})
-            return res.json({message:'succes',order})
-    }catch(error){
-        return res.json({message:'error',error:error.stack})
-    }
-    }
+export const addTraking = async (req, res) => {
+  try {
+    const { product } = req.params;
+    const { status, timeRemaining, totalPrice } = req.body;
+    const order = await orderModel.create({
+      userId: req.user._id,
+      orderTracking: [
+        {
+          productTracking: {
+            product: product,
+            status: status,
+            timeRemaining: timeRemaining,
+          },
+        },
+      ],
+      totalPrice: totalPrice,
+    });
+    return res.json({ message: "succes", order });
+  } catch (error) {
+    return res.json({ message: "error", error: error.stack });
+  }
+};
 
-    export const editStatus=async(req,res)=>{
-       try{ const {product}=req.params;
-        const{status,timeRemaining}=req.body;
-        const order= await orderModel.updateOne({_product:product},{status,timeRemaining});
-        return res.json({message:'succes',order})
-    }catch(error){
-        return res.json({message:'error',error:error.stack})
-    }
-    }
+export const editStatus = async (req, res) => {
+  try {
+    const { status, timeRemaining } = req.body;
+    const order = await orderModel.updateOne(
+      { product: req._product },
+      { status, timeRemaining }
+    );
+    return res.json({ message: "succes", order });
+  } catch (error) {
+    return res.json({ message: "error", error: error.stack });
+  }
+};
 
-    export const deleteTraking =async(req,res)=>{
-        try{const {product}=req.params;
-        const traking = await orderModel.deleteOne({_product:product})
-        return res.json({message:'succes',traking})
-    }catch(error){
-        return res.json({message:"error",error:error.stack})
-    }
-    }
+export const deleteTraking = async (req, res) => {
+  try {
+    const { product } = req.params;
+    const traking = await orderModel.deleteOne({ _product: product });
+    return res.json({ message: "succes", traking });
+  } catch (error) {
+    return res.json({ message: "error", error: error.stack });
+  }
+};
--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import * as OrderController from "./order.controller.js"
+import auth from "../../middleware/auth.middleware.js";
 const router =Router();
-
-router.get('/display',OrderController.DisplayTraking);
-router.post('/add/:product',OrderController.addTraking);
-router.patch('/update/:product',OrderController.editStatus);
+router.get('/display',auth,OrderController.DisplayTraking);
+router.post('/add/:product',auth,OrderController.addTraking);
+router.patch('/update',OrderController.editStatus);
 router.delete('/delete/:product',OrderController.deleteTraking);
-
 export default router;
