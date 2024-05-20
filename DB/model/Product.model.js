8b386729563008c5b872b7000f094917e029d2f8
mohammadAlawnah <152433070+mohammadAlawnah@users.noreply.github.com>
2024-05-20 21:00:47 +0300

    Update Product.model.js

--- DB/model/Product.model.js
+++ DB/model/Product.model.js
@@ -1,76 +1,73 @@
 import mongoose, { Schema, Types, model } from "mongoose";
+
 const productSchema = new Schema({
-    name : {
-        type : String,
-        unique : true,
-        require:true,
-        trim : true,  
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
     },
-
-    slug : {
-        type : String,
-        require : true,
+    slug: {
+        type: String,
+        required: true,
     },
-
-    description:{
-        type : String,
-        require:true,
+    description: {
+        type: String,
+        required: true,
     },
-
-    stock : {
-        type : Number,
-        default : 1,
+    stock: {
+        type: Number,
+        default: 1,
     },
-
-    price:{
-        type : Number,
-        require:true,
+    price: {
+        type: Number,
+        required: true,
     },
-    discount:{
-        type : Number,
-        default :0,
+    discount: {
+        type: Number,
+        default: 0,
     },
-    finalPrice:{
-        type:Number,
+    finalPrice: {
+        type: Number,
     },
-    mainImage:{
-        type : Object,
-        require:true,
+    mainImage: {
+        type: Object,
+        required: true,
     },
-    subImages:[{
-        type : Object,
+    subImages: [{
+        type: Object,
     }],
-
-    status : {
-        type : String,
-        default : 'Active',
-        enum :['Active','NotActive'],
+    status: {
+        type: String,
+        default: 'Active',
+        enum: ['Active', 'NotActive'],
     },
-    sizez :{
-        type : String,
-        enum:['s','m','lg','xl']
+    sizez: {
+        type: String,
+        enum: ['s', 'm', 'lg', 'xl']
     },
-    colors:[String],
-
-    categoryId:{
-        type:Types.ObjectId,
-        ref:'Category',
-        require:true,
+    colors: [String],
+    categoryId: {
+        type: Types.ObjectId,
+        ref: 'Category',
+        required: true,
     },
-
-    subcategoryId:{
-        type : Types.ObjectId,
-        ref : 'Subcategory',
-        require : true,
+    subcategoryId: {
+        type: Types.ObjectId,
+        ref: 'Subcategory',
+        required: true,
     },
+    createdBy: { type: Types.ObjectId, ref: 'User' },
+    updatedBy: { type: Types.ObjectId, ref: 'User' },
+    rate: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0,
+    },
+}, {
+    timestamps: true,
+});
 
-    createdBy : {type:Types.ObjectId,ref:'User'},
-    updatedBy : {type:Types.ObjectId,ref:'User'},
-
-},{
-    timestamps : true,
-})
-
-
-const productModel = model('Product',productSchema)
+const productModel = model('Product', productSchema);
 export default productModel;
