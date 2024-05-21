1d0eca940a0a2463a47f27c7a17f76680533052a
ReemaKusa <s12112910@stu.najah.edu>
2024-05-21 21:02:19 +0300

    adding the roles

---
+++ src/modules/commentsAndReview/commentReview.role.js
@@ -0,0 +1,9 @@
+import  { roles }  from "../../middleware/auth.middleware.js";
+
+export const endPoint = {
+
+    add : [roles.GeneralUser],
+    display : [roles.Admin,roles.Staff,roles.GeneralUser],
+    update : [roles.GeneralUser],
+    delete :[roles.Admin,roles.Staff,roles.GeneralUser]
+}
