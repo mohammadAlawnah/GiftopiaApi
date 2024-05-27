491ebb8cf8f62f9eaaae2862f1aaa45b7290b873
Muawiya-Ismael <116707240+Muawiya-Ismael@users.noreply.github.com>
2024-05-27 19:37:17 +0300

    Update cart.role.js
    adding this to cart.role.js                                                                   //this code written by muawiya ismail-->
    export const endPoints ={
        create:[roles.GeneralUser,roles.Admin],
        delete:[roles.GeneralUser,,roles.Admin]
    }//<--

--- src/modules/cart/cart.role.js
+++ src/modules/cart/cart.role.js
@@ -2,6 +2,6 @@ import {roles} from "../../middleware/auth.middleware.js"
 
 //this code written by muawiya ismail-->
 export const endPoints ={
-    create:[roles.GeneralUser],
-    delete:[roles.GeneralUser]
+    create:[roles.GeneralUser,roles.Admin],
+    delete:[roles.GeneralUser,,roles.Admin]
 }//<--
