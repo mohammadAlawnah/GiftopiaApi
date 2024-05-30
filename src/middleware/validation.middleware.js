6b6ebb0628a9fc58b1bb266cde64cd6a82b8ff1e
Rajaa-Ayyash <rajaaayyash43@gmail.com>
2024-05-30 19:09:11 +0300

    Add Validation

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -1,1 +1,20 @@
-// Test
+//Rajaa Ayyash
+const dataMethods = ['body','query','params'];
+const validation = (schema)=>{
+    return (req,res,next)=>{
+        const validationArray = [];
+        dataMethods.forEach(key=>{
+            if(schema[key]){
+                const validationResult = schema[key].validate(req[key],{abortEarly:false})
+                if(validationResult.error){
+                    validationArray.push(validationResult.error);
+                }
+            }
+        })
+        if(validationArray.length > 0){
+            return res.status(400).json({message : 'validation error',error:validationArray})
+        }
+        next();
+    }
+}
+export default validation;
